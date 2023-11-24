a8dec956c1478b542f92c304df3e9f3a97c25cd2
Verity Gregory <verity.gregory@gmail.com>
2023-11-24 22:18:08 +0000

    adds room state updates

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -1,29 +1,50 @@
 import { Room, Client } from "@colyseus/core";
-import { MyRoomState } from "./schema/MyRoomState";
+import { MyRoomState, Player } from "./schema/MyRoomState";
 
 export class MyRoom extends Room<MyRoomState> {
   maxClients = 4;
 
-  onCreate (options: any) {
+  onCreate(options: any) {
     this.setState(new MyRoomState());
 
-    this.onMessage("type", (client, message) => {
-      //
-      // handle "type" message
-      //
+    this.onMessage(0, (client, data) => {
+      // get reference to the player who sent the message
+      const player = this.state.players.get(client.sessionId);
+      const velocity = 2;
+
+      if (data.left) {
+        player.x -= velocity;
+      } else if (data.right) {
+        player.x += velocity;
+      }
+
+      if (data.up) {
+        player.y -= velocity;
+      } else if (data.down) {
+        player.y += velocity;
+      }
     });
   }
 
-  onJoin (client: Client, options: any) {
+  onJoin(client: Client, options: any) {
     console.log(client.sessionId, "joined!");
+    const mapWidth = 800;
+    const mapHeight = 600;
+
+    const player = new Player();
+
+    player.x = Math.random() * mapWidth;
+    player.y = Math.random() * mapHeight;
+
+    this.state.players.set(client.sessionId, player);
   }
 
-  onLeave (client: Client, consented: boolean) {
+  onLeave(client: Client, consented: boolean) {
     console.log(client.sessionId, "left!");
+    this.state.players.delete(client.sessionId);
   }
 
   onDispose() {
     console.log("room", this.roomId, "disposing...");
   }
-
 }
