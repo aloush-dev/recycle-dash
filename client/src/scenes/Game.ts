f2f8c584bdd13d80afe2eef4e06893a513c8729c
Verity Gregory <verity.gregory@gmail.com>
2023-11-24 21:44:00 +0000

    this commit adds unique player instances

--- client/src/scenes/Game.ts
+++ client/src/scenes/Game.ts
@@ -8,11 +8,37 @@ export default class Game extends Phaser.Scene {
   client = new Client("ws://localhost:2567");
   room!: Room;
 
+  playerEntities: { [sessionId: string]: any } = {};
+
   async create() {
     console.log("Joining Room");
     try {
       this.room = await this.client.joinOrCreate("my_room");
-      console.log("Joined successfully");
+
+      this.room.state.players.onAdd(
+        (player: { x: number; y: number }, sessionId: string | number) => {
+          console.log(
+            "A player has joined! Their unique session id is",
+            sessionId
+          );
+          const entity = this.physics.add.image(
+            player.x,
+            player.y,
+            "player_one"
+          );
+          this.playerEntities[sessionId] = entity;
+        }
+      );
+      this.room.state.players.onRemove(
+        (player: { x: number; y: number }, sessionId: string | number) => {
+          const entity = this.playerEntities[sessionId];
+          if (entity) {
+            entity.destroy();
+            delete this.playerEntities[sessionId];
+          }
+          console.log(`Player at session ${sessionId} has left the game`);
+        }
+      );
     } catch (e) {
       console.error(e);
     }
--- client/src/scenes/Preloader.ts
+++ client/src/scenes/Preloader.ts
@@ -4,7 +4,12 @@ export default class PreLoader extends Phaser.Scene {
   constructor() {
     super("preloader");
   }
-  preload() {}
+  preload() {
+    this.load.image(
+      "player_one",
+      "https://cdn.glitch.global/3e033dcd-d5be-4db4-99e8-086ae90969ec/ship_0001.png"
+    );
+  }
   create() {
     this.scene.start("game");
   }
