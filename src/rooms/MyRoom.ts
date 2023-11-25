9fa4843b1c50ebbc7381315ee0a03998492c3eb2
Mark Tyree <tyreemarka@gmail.com>
2023-11-25 22:27:24 +0000

    fixed brackets

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -23,45 +23,50 @@ export class MyRoom extends Room<MyRoomState> {
     this.setState(new MyRoomState());
     this.setUpCans();
     this.setUpTrash();
-    
+
     this.onMessage(0, (client, input) => {
-
-    const locations: { x: number; y: number }[] = [];
-    for (let i = 4; i > 0; i--) {
-      const x = WIDTH / 2;
-      const y = i * 100;
-      locations.push({ x, y });
-    }
-    locations.sort(() => Math.random() - 0.5);
-    this.state.trashCans.set("0", new PaperCan(locations[0].x, locations[0].y));
-    this.state.trashCans.set(
-      "1",
-      new PlasticCan(locations[1].x, locations[1].y)
-    );
-    this.state.trashCans.set("2", new GlassCan(locations[2].x, locations[2].y));
-    this.state.trashCans.set(
-      "3",
-      new NonRecyclable(locations[3].x, locations[3].y)
-    );
-
-    this.onMessage("updatePlayer", (client, input) => {
-      const player = this.state.players.get(client.sessionId);
-      const velocity = 2;
-
-      player.animation = input.animation;
-      if (input.left) {
-        player.x -= velocity;
-      } else if (input.right) {
-        player.x += velocity;
+      const locations: { x: number; y: number }[] = [];
+      for (let i = 4; i > 0; i--) {
+        const x = WIDTH / 2;
+        const y = i * 100;
+        locations.push({ x, y });
       }
-      if (input.up) {
-        player.y -= velocity;
-      } else if (input.down) {
-        player.y += velocity;
-      }
+      locations.sort(() => Math.random() - 0.5);
+      this.state.trashCans.set(
+        "0",
+        new PaperCan(locations[0].x, locations[0].y)
+      );
+      this.state.trashCans.set(
+        "1",
+        new PlasticCan(locations[1].x, locations[1].y)
+      );
+      this.state.trashCans.set(
+        "2",
+        new GlassCan(locations[2].x, locations[2].y)
+      );
+      this.state.trashCans.set(
+        "3",
+        new NonRecyclable(locations[3].x, locations[3].y)
+      );
+
+      this.onMessage("updatePlayer", (client, input) => {
+        const player = this.state.players.get(client.sessionId);
+        const velocity = 2;
+
+        player.animation = input.animation;
+        if (input.left) {
+          player.x -= velocity;
+        } else if (input.right) {
+          player.x += velocity;
+        }
+        if (input.up) {
+          player.y -= velocity;
+        } else if (input.down) {
+          player.y += velocity;
+        }
+      });
     });
   }
-
   onJoin(client: Client, options: any) {
     console.log(client.sessionId, "joined!");
     const mapWidth = WIDTH;
