232ca9bbea8713500a965db878e97fed07a2fd81
Verity Gregory <verity.gregory@gmail.com>
2023-11-25 12:19:33 +0000

    adds dynamic animations to four players

--- client/src/scenes/Preloader.ts
+++ client/src/scenes/Preloader.ts
@@ -1,76 +1,107 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 export default class PreLoader extends Phaser.Scene {
   constructor() {
-    super("preloader");
+    super('preloader');
   }
 
   preload() {
     this.load.spritesheet(
-      "playerSheet",
-      "https://i.ibb.co/rbjPCCj/Characters-V3-Colour-3.png",
+      'playerSheet',
+      'https://i.ibb.co/rbjPCCj/Characters-V3-Colour-3.png',
       { frameWidth: 80, frameHeight: 80 }
     );
   }
   create() {
-    this.anims.create({
-      key: "down-idle-0",
-      frames: [{ key: "playerSheet", frame: 156 }],
-    });
-    this.anims.create({
-      key: "up-idle-0",
-      frames: [{ key: "playerSheet", frame: 157 }],
-    });
+    const startPositions = {
+      downIdle: 156,
+      upIdle: 157,
+      leftIdle: 159,
+      rightIdle: 158,
+      downWalkStart: 160,
+      downWalkEnd: 161,
+      upWalkStart: 162,
+      upWalkEnd: 163,
+      leftWalkStart: 166,
+      leftWalkEnd: 167,
+      rightWalkStart: 164,
+      rightWalkEnd: 165,
+    };
+    for (let i = 0; i < 4; i++) {
+      this.anims.create({
+        key: `down-idle-${i}`,
+        frames: [{ key: `playerSheet`, frame: startPositions.downIdle }],
+      });
+      this.anims.create({
+        key: `up-idle-${i}`,
+        frames: [{ key: `playerSheet`, frame: startPositions.upIdle }],
+      });
 
-    this.anims.create({
-      key: "left-idle-0",
-      frames: [{ key: "playerSheet", frame: 159 }],
-    });
+      this.anims.create({
+        key: `left-idle-${i}`,
+        frames: [{ key: `playerSheet`, frame: startPositions.leftIdle }],
+      });
 
-    this.anims.create({
-      key: "right-idle-0",
-      frames: [{ key: "playerSheet", frame: 158 }],
-    });
+      this.anims.create({
+        key: `right-idle-${i}`,
+        frames: [{ key: `playerSheet`, frame: startPositions.rightIdle }],
+      });
 
-    this.anims.create({
-      key: "down-walk-0",
-      frames: this.anims.generateFrameNumbers("playerSheet", {
-        start: 160,
-        end: 161,
-      }),
-      frameRate: 8,
-      repeat: -1,
-    });
+      this.anims.create({
+        key: `down-walk-${i}`,
+        frames: this.anims.generateFrameNumbers(`playerSheet`, {
+          start: startPositions.downWalkStart,
+          end: startPositions.downWalkEnd,
+        }),
+        frameRate: 8,
+        repeat: -1,
+      });
 
-    this.anims.create({
-      key: "up-walk-0",
-      frames: this.anims.generateFrameNumbers("playerSheet", {
-        start: 162,
-        end: 163,
-      }),
-      frameRate: 8,
-      repeat: -1,
-    });
+      this.anims.create({
+        key: `up-walk-${i}`,
+        frames: this.anims.generateFrameNumbers(`playerSheet`, {
+          start: startPositions.upWalkStart,
+          end: startPositions.upWalkEnd,
+        }),
+        frameRate: 8,
+        repeat: -1,
+      });
 
-    this.anims.create({
-      key: "left-walk-0",
-      frames: this.anims.generateFrameNumbers("playerSheet", {
-        start: 166,
-        end: 167,
-      }),
-      frameRate: 8,
-      repeat: -1,
-    });
+      this.anims.create({
+        key: `left-walk-${i}`,
+        frames: this.anims.generateFrameNumbers(`playerSheet`, {
+          start: startPositions.leftWalkStart,
+          end: startPositions.leftWalkEnd,
+        }),
+        frameRate: 8,
+        repeat: -1,
+      });
 
-    this.anims.create({
-      key: "right-walk-0",
-      frames: this.anims.generateFrameNumbers("playerSheet", {
-        start: 164,
-        end: 165,
-      }),
-      frameRate: 8,
-      repeat: -1,
-    });
-    this.scene.start("game");
+      this.anims.create({
+        key: `right-walk-${i}`,
+        frames: this.anims.generateFrameNumbers(`playerSheet`, {
+          start: startPositions.rightWalkStart,
+          end: startPositions.rightWalkEnd,
+        }),
+        frameRate: 8,
+        repeat: -1,
+      });
+
+      startPositions.downIdle += 12;
+      startPositions.upIdle += 12;
+      startPositions.leftIdle += 12;
+      startPositions.rightIdle += 12;
+
+      startPositions.downWalkStart += 12;
+      startPositions.downWalkEnd += 12;
+      startPositions.upWalkStart += 12;
+      startPositions.upWalkEnd += 12;
+      startPositions.leftWalkStart += 12;
+      startPositions.leftWalkEnd += 12;
+      startPositions.rightWalkStart += 12;
+      startPositions.rightWalkEnd += 12;
+    }
+
+    this.scene.start('game');
   }
 }
